d567cac5cf2043d994c0068a6f481fcfb9c2cbde
Fernando Lange <pymozambique@gmail.com>
2025-09-24 22:44:05 +0200

    Change settings.py

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n0&g(316@gl8g=i89c6-ahrmqc*pmtwh@@-)g!!)mqrv3%58zq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['api-blog-w6l1.onrender.com']
 
